6be0ddc942f379c3a3b702048040658222dafa86
Dhruvil <lakhtariadhruvil123@gmail.com>
2021-05-12 16:46:05 +0530

    Added reset functionality and draw status

--- src/index.js
+++ src/index.js
@@ -15,7 +15,6 @@ function Square(props) {
     );
 }
 
-
 class Col extends React.Component {
 
     renderSquare(i) {
@@ -51,17 +50,30 @@ class Board extends React.Component {
         }
     }
 
+    handleReset()
+    {   const val = this.state.boardValue.slice();
+        for(let c = 0;c<7;c++)
+        {
+            for(let r = 0;r<6;r++)
+            {
+                val[c][r] = null;
+            }
+        }
+        this.setState({boardValue:val,
+            redIsNext : true,
+             winner : null });
+    }
+
     handleClick(col) {
         /* Check if there exists a winner 
         If winner is not null then return since game is over 
         and any other click should not continue playing*/
-        if(this.state.winner !== null)
+        if(this.state.winner !== null )
         return;
 
         const val = this.state.boardValue.slice();
         const curPlayer = this.state.redIsNext? 'Red' : 'Yellow';
         let findResult;
-        
 
         let level = null;
        
@@ -76,8 +88,6 @@ class Board extends React.Component {
             }
             if(level === null)
             return;
-        
-        
 
         /*THE BELOW LINE CHECKS IF GAME IS OVER NOW
         if it is we know that the curPlayer has won since 
@@ -85,11 +95,9 @@ class Board extends React.Component {
         findResult = gameOver(val);
         var curWinner = findResult? curPlayer : null;
 
-        
         this.setState({boardValue:val,
                        redIsNext : !this.state.redIsNext,
                         winner : curWinner });
-        
     }
 
     renderCol(i) {
@@ -101,7 +109,9 @@ class Board extends React.Component {
 
     render() {
         const nextPlayer = this.state.redIsNext ? 'RED' : 'YELLOW'; 
-        var status = this.state.winner ? 'WINNER : ' + this.state.winner: 'NEXT PLAYER : ' + nextPlayer;
+        const currVal = this.state.boardValue.slice();
+        var checkFull = isFull(currVal);
+        var status = this.state.winner ? 'WINNER : ' + this.state.winner : checkFull ? 'DRAW': 'NEXT PLAYER : ' + nextPlayer;
         
         return (
             <div className="game">
@@ -115,6 +125,11 @@ class Board extends React.Component {
                 {this.renderCol(5)}
                 {this.renderCol(6)}
                 </div>
+                <div className = "reset">
+                <button className ="reset btn"  onClick = {() => this.handleReset()}>
+                    RESET
+                </button>
+                </div>
             </div>
         );
     }
@@ -127,12 +142,27 @@ function check(a, b, c, d) {
     return false;
 }
 
+
+function isFull(board)
+{
+   for(let c = 0;c < 7; c++)
+   {
+       for(let r = 0;r < 6;r++)
+       {
+           if(board[c][r] === null)
+           return false;
+       }
+   } 
+   return true;
+}
+
 /*
 Function to check if the game is over
 check if there exists a winner
 take all possible combinations of 4 that can win
 and pass it to the check function to see if they are all the same
 */
+
 function gameOver(board) {
     //VERTICAL
     for (let c = 0; c < 7; c++)
@@ -179,6 +209,7 @@ class Game extends React.Component {
             <div>
                 <h1>Connect4</h1>
                 <Board/>
+                
             </div>
         );
     }
